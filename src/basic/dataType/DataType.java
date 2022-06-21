718c9df382c62b8d17168891796938ffda782edd
tsqqqqqq <563068223@qq.com>
2022-06-22 00:20:50 +0800

    同步服务器代码

---
+++ src/basic/dataType/DataType.java
@@ -0,0 +1,10 @@
+package dataType;
+public class DataType{ 
+
+    public DataType() {
+    }
+    public void getInt() {
+    	System.out.println(">>>> Integer min >>>>>" + Integer.MIN_VALUE);
+    	System.out.println(">>>> Integer max >>>>>" + Integer.MAX_VALUE);
+    }
+}
---
+++ src/basic/dataType/Main.java
@@ -0,0 +1,11 @@
+package dataType;
+
+
+
+public class Main {
+   
+    public static void main(String[] args) {
+       DataType d = new DataType();
+       d.getInt();
+    }
+}
